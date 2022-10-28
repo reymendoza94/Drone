d6605cd182bfbeed37c39aa01ae9967c70f1d0ab
reymendoza94 <reymendoza94>
2022-10-28 09:18:06 -0400

    build view create_ medication and create_dispatch_controler

--- drone/core/forms.py
+++ drone/core/forms.py
@@ -1,5 +1,5 @@
 from dataclasses import fields
-from .models import Drone
+from .models import Drone, Medication, DispatchController
 from django import forms
 
 class DroneForm(forms.ModelForm):
@@ -7,4 +7,17 @@ class DroneForm(forms.ModelForm):
     class Meta:
         model = Drone
         fields = "__all__"
+
+
+class MedicationForm(forms.ModelForm):
+    
+    class Meta:
+        model = Medication
+        fields = "__all__"
         
+
+class DispatchControllerForm(forms.ModelForm):
+    
+    class Meta:
+        model = DispatchController
+        fields = "__all__"
--- drone/core/urls.py
+++ drone/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import create_drone
+from .views import create_drone, create_medication
 
 urlpatterns = [
     path('create_drone/', create_drone, name="create_drone"),
+    path('create_medication/', create_medication, name="create_medication")
 ]
