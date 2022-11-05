2a87e73ad832080ece45907ca0360d33733709b8
reymendoza94 <reymendoza94>
2022-11-05 10:16:52 -0400

    List medications in view create_ispatch_controler

--- drone/core/forms.py
+++ drone/core/forms.py
@@ -1,6 +1,7 @@
 from dataclasses import fields
-from .models import Drone, Medication, DispatchController
+from .models import Drone, Medication
 from django import forms
+
 
 class DroneForm(forms.ModelForm):
     
@@ -16,11 +17,11 @@ class MedicationForm(forms.ModelForm):
         fields = "__all__"
         
 
-class DispatchControllerForm(forms.ModelForm):
+# class DispatchControllerForm(forms.ModelForm):
     
-    class Meta:
-        model = DispatchController
-        fields = "__all__"
+#     class Meta:
+#         model = DispatchController
+#         fields = "__all__"
 
     # def save(self) -> None:
     #     print(self.data)
